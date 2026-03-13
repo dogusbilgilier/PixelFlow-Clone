d49f33ade5da1c1228f4f57ac11bc7959e8aff17
Doğuş Bilgilier <bildogus@icloud.com>
2026-03-13 06:46:34 +0300

    Data classes removed from LeveData.cs & ShooterTargetData class removed from Shooter.cs

---
+++ Assets/Scripts/LevelEditor/LevelData/ShooterData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class ShooterData
+{
+    public int ID;
+    public int BulletCount;
+    public int ColorId;
+    public int LinkedShooterID;
+    public Vector2Int Coordinates;
+    public bool IsHidden;
+
+    public ShooterData(int id, int bulletCount, int colorId, int linkedShooterId, Vector2Int coordinates, bool isHidden)
+    {
+        ID = id;
+        BulletCount = bulletCount;
+        ColorId = colorId;
+        LinkedShooterID = linkedShooterId;
+        Coordinates = coordinates;
+        IsHidden = isHidden;
+    }
+}
---
+++ Assets/Scripts/LevelEditor/LevelData/TargetData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class TargetData
+{
+    public Vector2Int Coordinates;
+    public int ColorId;
+
+    public TargetData(Vector2Int coordinates, int colorId)
+    {
+        Coordinates = coordinates;
+        ColorId = colorId;
+    }
+}
